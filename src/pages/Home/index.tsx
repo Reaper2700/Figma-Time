f6a4e51e48f3c3140f22ff7dacd1e267083a2238
Rafael Pereira <rafaell_b.p@edu.unifor.br>
2024-10-25 00:08:55 -0300

    second commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,14 +9,26 @@ export function Home() {
                     <label> Vou trabalhar em</label>
                     <TaskInput 
                     id="task" 
+                    list="task-sugestions"
                     placeholder="Dê um nome para seu projeto"
                     />
+
+                    <datalist id="task-sugestions">
+                        <option value="Projeto 1"/>
+                        <option value="Projeto 2"/>
+                        <option value="Projeto 3"/>
+                        <option value="Projeto 4"/>
+                        <option value="Projeto 5"/>
+                    </datalist>
 
                     <label htmlFor="">durante</label>
                     <MinutesAmountInput 
                     type="number" 
                     id="minutesAmount" 
                     placeholder="00" 
+                    step={5}
+                    min={5}
+                    max={95}
                     />
 
                     <span>minutos.</span>
